d55145e8af5ba06f45ce5005a0298b5a5927da44
Vladimir Stojcheski <stojcheskivladimir@gmail.com>
2024-02-14 04:40:47 +0100

    bug fix

--- maven.sh
+++ maven.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Maven settings
 NEXUS_USERNAME="admin"
 NEXUS_URL="http://localhost:8090/repository/maven-releases/"
 
-# Maven commands
 MVN="mvn"
 MVN_CLEAN="$MVN clean"
 MVN_INSTALL="$MVN install"
@@ -13,16 +11,12 @@ grep -r "product-catalog-0.0.1.jar" .
 echo "Current working directory: $(pwd)"
 echo "Contents of parent directory:"
 find / -type d -name "product"
-# Path to the product-catalog-model project directory
 PROJECT_DIR="./product\ catalog/src/main/java/mk/ukim/finki/soa/productcatalog/model"
 
-# Change to the project directory
 cd "$PROJECT_DIR" || exit
 
-# Build the project
 echo "Building the project..."
 $MVN_CLEAN && $MVN_INSTALL
 
-# Deploy the artifact to Nexus
 echo "Deploying the artifact to Nexus..."
 $MVN_DEPLOY -Dmaven.test.skip=true -DaltDeploymentRepository=nexus::default::"$NEXUS_URL" -DrepositoryId=nexus -Durl="$NEXUS_URL" -Dusername="$NEXUS_USERNAME"
--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# Wait for PostgreSQL to be ready
 while ! pg_isready -h database -p 5432 -U postgres -d postgres; do
   echo "Waiting for PostgreSQL to be ready..."
   sleep 1
